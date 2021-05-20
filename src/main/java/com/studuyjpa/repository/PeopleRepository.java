971f9dfc5bc90e911fc1eb70ec55147ba244351e
ylt <2262018714@qq.com>
2021-05-20 11:01:43 +0800

    add PeopleRepository

---
+++ src/main/java/com/studuyjpa/repository/PeopleRepository.java
@@ -0,0 +1,21 @@
+package com.studuyjpa.repository;
+
+import com.studuyjpa.entity.People;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author ylt
+ * @date 2021/5/20 10:39
+ */
+@Repository
+public interface PeopleRepository extends JpaRepository<People, Long> {
+    /**
+     * findMaxLeng
+     * @return Integer
+     */
+    @Query(value = "select max(People.length) from People "
+            ,nativeQuery = true)
+    Integer findMaxLength();
+}
